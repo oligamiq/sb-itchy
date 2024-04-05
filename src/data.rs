0965bd6459be891292699069161d162e023fa9a4
oligami-0424 <s20032@tokyo.kosen-ac.jp>
2024-04-06 05:20:54 +0900

    patch

--- src/data.rs
+++ src/data.rs
@@ -1,30 +1,30 @@
 use crate::uid::Uid;
-use sb_sbity::{list::List, value::{Value, ValueWithBool}, variable::Variable};
+use sb_sbity::{list::List, value::ValueWithBool, variable::Variable};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct VariableBuilder {
-    pub value: Value,
+    pub value: ValueWithBool,
     /// Cloud variable can only store number. Becareful!
     pub is_cloud_variable: bool,
 }
 
 impl VariableBuilder {
-    pub fn new(starting_value: Value) -> VariableBuilder {
+    pub fn new(starting_value: ValueWithBool) -> VariableBuilder {
         VariableBuilder {
             value: starting_value,
             is_cloud_variable: false,
         }
     }
 
-    pub fn new_cloud_variable(starting_value: Value) -> VariableBuilder {
-        debug_assert!(matches!(starting_value, Value::Number(_)));
+    pub fn new_cloud_variable(starting_value: ValueWithBool) -> VariableBuilder {
+        debug_assert!(matches!(starting_value, ValueWithBool::Number(_)));
         VariableBuilder {
             value: starting_value,
             is_cloud_variable: true,
         }
     }
 
-    pub fn set_value(&mut self, value: Value) -> &mut Self {
+    pub fn set_value(&mut self, value: ValueWithBool) -> &mut Self {
         self.value = value;
         self
     }
